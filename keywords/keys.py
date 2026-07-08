e2b6f18d3f8209d1b4b98df389a3bf67e37f55a2
Anuj04432 <anujwagmore835@gmail.com>
2026-07-08 21:36:02 +0530

    Added keywords

---
+++ keywords/keys.py
@@ -0,0 +1,12 @@
+keywords = {"DataAnalyst":["Excel","PowerBI","Pandas","SQL","Tabalue","Numpy"],
+            "MachineLearning Engineer":["machine learning", 'tensorflow','pytorch','ai'],
+            "Web Developer":["html","css",'javascript','react','node','frontend',"backend","fullstack"],
+            "Python Developer":['python','djanjo','flask','fastapi','pydantic'],
+            "AI Engineer":['llm','genai','rag',"langchain"]
+}
+
+
+            
+
+
+
