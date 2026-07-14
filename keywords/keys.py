2071741777d2a7e2ce27ecfe29018711d4e69f7a
Anuj04432 <anujwagmore835@gmail.com>
2026-07-14 21:21:08 +0530

    Added imp keywords in dict

--- keywords/keys.py
+++ keywords/keys.py
@@ -1,12 +1,3 @@
-keywords = {"DataAnalyst":["Excel","PowerBI","Pandas","SQL","Tabalue","Numpy"],
-            "MachineLearning Engineer":["machine learning", 'tensorflow','pytorch','ai'],
-            "Web Developer":["html","css",'javascript','react','node','frontend',"backend","fullstack"],
-            "Python Developer":['python','djanjo','flask','fastapi','pydantic'],
-            "AI Engineer":['llm','genai','rag',"langchain"]
-}
-
-
-            
 job_keywords = {
     "Data Analyst": [
         "SQL", "Python", "Excel", "Power BI", "Tableau",
